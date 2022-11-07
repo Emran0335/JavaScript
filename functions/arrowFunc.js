dabeead550511afaacb6524243dbe249a126706a
Emran Hossain <emranhossain@Emrans-Mac-mini.local>
2022-11-07 21:11:28 +0600

    learning JavaScript

---
+++ functions/arrowFunc.js
@@ -0,0 +1,9 @@
+// Arrow Function
+// Arrow function is an alternative to write a function but there are some differences between function declaration and arrow function.
+
+// Arrow function uses arrow instead of the keyword function to declare a function. Let us see arrow function.
+
+const square = n => {
+    return n * n;
+}
+console.log(square(2)); // 4
