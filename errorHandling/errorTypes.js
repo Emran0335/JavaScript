48935a12812b4aa28056d1f4445218effb00f0ed
Emran Hossain <emranhossain@Emrans-Mac-mini.local>
2022-11-12 11:07:46 +0600

    learning JavaScript

---
+++ errorHandling/errorTypes.js
@@ -0,0 +1,17 @@
+// Error Handling
+// JavaScript is a loosely-typed language. Sometimes we will get a runtime error when we try to access an undefined variable or call undefined function etc.
+
+// JavaScript is similar to Python or Java which provides an error handling mechanism to catch runtime errors using try-catch-finally block.
+
+// try {
+//     code that may throw an error
+// } catch (error) {
+//     code to be executed if an error occurs
+// } finally {
+//     code to be executed regardless of an error occurs or not
+// }
+
+
+// try: wrap suspicious code that may throw an error in try block. The try statement allows us to define a block of code to be tested for errors while it is being executed.
+// catch: write code to do something in catch block when an error occurs. The catch block can have parameters that will give us error information. Catch block is used to log an error or display specific messages to the user.
+// finally: finally block will always be 
