48b64d5aed9e31903dfd950d4047659b03e3c8d4
Emran Hossain <emranhossain@Emrans-Mac-mini.local>
2022-11-19 21:52:47 +0600

    learning JavaScript

--- redux-toolkit/reduxFetchDataWithRedux-Thunk.js
+++ redux-toolkit/reduxFetchDataWithRedux-Thunk.js
@@ -93,7 +93,10 @@ store.subscribe(() => {
 
 store.dispatch(fetchData()); // dispatch() only expect any function with return value as an object. But fetchData() function returns a function as applyMiddleware(thunk), thunk works to handle it to do so.
 
+// we have to practice it again and again to get the concept clear
+
 /*
+
     applyMiddleware(thunk) is not used..... the problem will be like below.
-    Error: Actions must be plain objects. Instead, the actual type was: 'function'. You may need to add middleware to your store setup to handle dispatching other values, such as 'redux-thunk' to handle dispatching functions. 
+    Error: Actions must be plain objects. Instead, the actual type was: 'function'. You may need to add middleware to your store setup to handle dispatching other values, such as 'redux-thunk' to handle dispatching functions.
 */
