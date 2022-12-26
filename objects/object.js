29d42477b300dbd260bdbb5c4eb0d448811b879e
Emran Hossain <emranhossain@Emrans-Mac-mini.local>
2022-12-26 21:52:39 +0600

    backend is going to be learned

--- objects/object.js
+++ objects/object.js
@@ -110,3 +110,22 @@ function reverseTokens(token) {
 
 const result = reverseTokens(tokensDark);
 console.log(result);
+
+// It looks like you are trying to create a new object with the keys and values of an existing object reversed.
+// To do this, you can use a combination of the Object.keys() and Object.values() methods to get the keys and values of the object, and then iterate over them using a for loop to create a new object with the reversed keys and values.
+
+// Here's an example of how to do this:
+// Copy code
+const object = { a: 1, b: 2, c: 3 };
+
+const keys = Object.keys(object);
+const values = Object.values(object);
+const length = keys.length;
+const reversedObj = {};
+
+for (let i = 0; i < length; i++) {
+  reversedObj[keys[i]] = values[length - i - 1];
+}
+
+console.log(reversedObj);  // Output: { a: 3, b: 2, c: 1 }
+// Keep in mind that this will only work for objects that have enumerable own properties. If the object has inherited properties or non-enumerable own properties, they will not be included in the reversed object.
