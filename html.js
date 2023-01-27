7403ecdd73b976d607c5ef13ddada8954a55f44c
Emran Hossain <emranhossain@Emrans-Mac-mini.local>
2023-01-27 20:29:58 +0600

    all in javascript

---
+++ html.js
@@ -0,0 +1,74 @@
+CSS Defaults
+#
+Browsers assign default values to properties on HTML elements. For example, the <body> element has a default margin of 10px. Unfortunately, there are inconsistencies between the different browsers.
+
+Below are some elements and their default property values.
+
+ELEMENT	                DEFAULTS
+<area>	                display: none;
+
+<article>	            display: block;
+
+<blockquote>	        display: block;
+
+                        margin-top: 1em;
+                        margin-bottom: 1em;
+                        margin-left: 50px;
+                        margin-right: 50px;
+
+<body>	                display: block;
+                        margin: 10px;
+
+<del>	                text-decoration: line-through;
+
+<fieldset>	            display: block;
+                        margin-left: 3px;
+                        margin-right: 3px;
+                        padding-top: 1em;
+                        padding-bottom: 1em;
+                        padding-left: 0.5em;
+                        padding-right: 0.5em;
+                        border: 1px solid #ccc;
+
+<form>	                display: inline-block;
+                        margin-top: 0em;
+
+<h1>	                font-weight: bold;
+
+<header>	            display: block;
+
+<i>	                    font-style: italic;
+
+<img>	                display: inline-block;
+
+<legend>	            display: block;
+                        padding-left: 3px;
+                        padding-right: 3px;
+
+<ol>	                list-style-type: upper-roman;
+
+<p>	                    margin-left: 20px;
+
+<pre>	                display: block;
+                        font-family: monospace;
+                        white-space: pre;
+
+<script>	            display: none;
+
+<strong>	            font-weight: bold;
+
+<sup>	                vertical-align: super;
+                        font-size: smaller;
+
+<table>	                border-collapse: collapse;
+                        border: 1px solid black;
+
+<td>	                display: table-cell;
+                        vertical-align: inherit;
+
+<th>	                display: table-cell;
+                        vertical-align: inherit;
+                        font-weight: bold;
+                        text-align: center;
+                        
+<ul>	                list-style-type: circle;
